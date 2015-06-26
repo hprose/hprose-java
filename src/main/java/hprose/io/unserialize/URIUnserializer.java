06209250b536f16e03bab65b958b4a92c1c66903
andot <mabingyao@gmail.com>
2015-06-27 01:37:44 +0800

    Improved URI Unserializer

--- src/main/java/hprose/io/unserialize/URIUnserializer.java
+++ src/main/java/hprose/io/unserialize/URIUnserializer.java
@@ -12,13 +12,14 @@
  *                                                        *
  * URL unserializer class for Java.                       *
  *                                                        *
- * LastModified: Jun 24, 2015                             *
+ * LastModified: Jun 27, 2015                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
 
 package hprose.io.unserialize;
 
+import static hprose.io.HproseTags.TagEmpty;
 import static hprose.io.HproseTags.TagNull;
 import static hprose.io.HproseTags.TagRef;
 import static hprose.io.HproseTags.TagString;
@@ -33,53 +34,65 @@ final class URIUnserializer implements HproseUnserializer {
 
     public final static URIUnserializer instance = new URIUnserializer();
 
-    private static URI toURI(String s, Type type) throws IOException {
+    private static URI toURI(String s) throws IOException {
         try {
             return new URI(s);
         }
         catch (URISyntaxException e) {
-            throw ValueReader.castError("String: '" + s + "'", type);
+            throw ValueReader.castError("String: '" + s + "'", URI.class);
         }
     }
-    public final Object read(HproseReader reader, ByteBuffer buffer, Class<?> cls, Type type) throws IOException {
+
+    final static URI readURI(HproseReader reader, ByteBuffer buffer) throws IOException {
+        URI u = toURI(ValueReader.readString(buffer));
+        reader.refer.set(u);
+        return u;
+    }
+
+    final static URI readURI(HproseReader reader, InputStream stream) throws IOException {
+        URI u = toURI(ValueReader.readString(stream));
+        reader.refer.set(u);
+        return u;
+    }
+
+    private static URI toURI(Object obj) throws IOException {
+        if (obj instanceof URI) {
+            return (URI)obj;
+        }
+        if (obj instanceof char[]) {
+            return toURI(new String((char[])obj));
+        }
+        return toURI(obj.toString());
+    }
+
+    final static URI read(HproseReader reader, ByteBuffer buffer) throws IOException {
         int tag = buffer.get();
-        if (tag == TagNull) return null;
-        if (tag == TagString) {
-            URI uri = toURI(ValueReader.readString(buffer), type);
-            reader.refer.set(uri);
-            return uri;
+        switch(tag) {
+            case TagNull:
+            case TagEmpty: return null;
+            case TagString: return readURI(reader, buffer);
+            case TagRef: return toURI(reader.readRef(buffer));
+            default: throw ValueReader.castError(reader.tagToString(tag), URI.class);
         }
-        if (tag == TagRef) {
-            Object obj = reader.readRef(buffer);
-            if (obj instanceof URI) {
-                return obj;
-            }
-            if (obj instanceof char[]) {
-                return toURI(new String((char[])obj), type);
-            }
-            return toURI(obj.toString(), type);
+    }
+
+    final static URI read(HproseReader reader, InputStream stream) throws IOException {
+        int tag = stream.read();
+        switch(tag) {
+            case TagNull:
+            case TagEmpty: return null;
+            case TagString: return readURI(reader, stream);
+            case TagRef: return toURI(reader.readRef(stream));
+            default: throw ValueReader.castError(reader.tagToString(tag), URI.class);
         }
-        throw ValueReader.castError(reader.tagToString(tag), URI.class);
+    }
+
+    public final Object read(HproseReader reader, ByteBuffer buffer, Class<?> cls, Type type) throws IOException {
+        return read(reader, buffer);
     }
 
     public final Object read(HproseReader reader, InputStream stream, Class<?> cls, Type type) throws IOException {
-        int tag = stream.read();
-        if (tag == TagNull) return null;
-        if (tag == TagString) {
-            URI url = toURI(ValueReader.readString(stream), type);
-            reader.refer.set(url);
-            return url;
-        }
-        if (tag == TagRef) {
-            Object obj = reader.readRef(stream);
-            if (obj instanceof URI) {
-                return obj;
-            }
-            if (obj instanceof char[]) {
-                return toURI(new String((char[])obj), type);
-            }
-            return toURI(obj.toString(), type);
-        }
-        throw ValueReader.castError(reader.tagToString(tag), URI.class);
+        return read(reader, stream);
     }
+
 }

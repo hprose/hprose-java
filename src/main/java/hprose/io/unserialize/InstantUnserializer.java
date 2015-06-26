b7cee501a3ba0103304ac3370af28e8cebbc00cb
andot <mabingyao@gmail.com>
2015-06-27 01:35:42 +0800

    Improved Instant Unserializer

--- src/main/java/hprose/io/unserialize/InstantUnserializer.java
+++ src/main/java/hprose/io/unserialize/InstantUnserializer.java
@@ -12,7 +12,7 @@
  *                                                        *
  * Instant unserializer class for Java.                   *
  *                                                        *
- * LastModified: Jun 26, 2015                             *
+ * LastModified: Jun 27, 2015                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
@@ -44,6 +44,9 @@ final class InstantUnserializer implements HproseUnserializer, HproseTags {
     private static Instant toInstant(Object obj) {
         if (obj instanceof DateTime) {
             return toInstant((DateTime)obj);
+        }
+        if (obj instanceof char[]) {
+            return Instant.parse(new String((char[])obj));
         }
         return Instant.parse(obj.toString());
     }

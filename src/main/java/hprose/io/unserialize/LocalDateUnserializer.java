f00250e32a3873a0abb0443c013364e295451308
andot <mabingyao@gmail.com>
2015-06-26 22:41:43 +0800

    Improved LocalDate unserializer

--- src/main/java/hprose/io/unserialize/LocalDateUnserializer.java
+++ src/main/java/hprose/io/unserialize/LocalDateUnserializer.java
@@ -44,50 +44,38 @@ final class LocalDateUnserializer implements HproseUnserializer, HproseTags {
 
     final static LocalDate read(HproseReader reader, ByteBuffer buffer) throws IOException {
         int tag = buffer.get();
-        if (tag == TagDate) return toLocalDate(DefaultUnserializer.readDateTime(reader, buffer));
-        if (tag == TagTime) return toLocalDate(DefaultUnserializer.readTime(reader, buffer));
-        if (tag == TagNull || tag == TagEmpty) return null;
-        if (tag == TagRef) return toLocalDate(reader.readRef(buffer));
         switch (tag) {
-            case '0': return LocalDate.ofEpochDay(0l);
-            case '1': return LocalDate.ofEpochDay(1l);
-            case '2': return LocalDate.ofEpochDay(2l);
-            case '3': return LocalDate.ofEpochDay(3l);
-            case '4': return LocalDate.ofEpochDay(4l);
-            case '5': return LocalDate.ofEpochDay(5l);
-            case '6': return LocalDate.ofEpochDay(6l);
-            case '7': return LocalDate.ofEpochDay(7l);
-            case '8': return LocalDate.ofEpochDay(8l);
-            case '9': return LocalDate.ofEpochDay(9l);
+            case TagDate: return toLocalDate(DefaultUnserializer.readDateTime(reader, buffer));
+            case TagTime: return toLocalDate(DefaultUnserializer.readTime(reader, buffer));
+            case TagNull:
+            case TagEmpty: return null;
+            case TagString: return LocalDate.parse(StringUnserializer.readString(reader, buffer));
+            case TagRef: return toLocalDate(reader.readRef(buffer));
+        }
+        if (tag >= '0' && tag <= '9') return LocalDate.ofEpochDay(tag - '0');
+        switch (tag) {
             case TagInteger:
             case TagLong: return LocalDate.ofEpochDay(ValueReader.readLong(buffer));
             case TagDouble: return LocalDate.ofEpochDay(Double.valueOf(ValueReader.readDouble(buffer)).longValue());
-            case TagString: return LocalDate.parse(StringUnserializer.readString(reader, buffer));
             default: throw ValueReader.castError(reader.tagToString(tag), LocalDate.class);
         }
     }
 
     final static LocalDate read(HproseReader reader, InputStream stream) throws IOException {
         int tag = stream.read();
-        if (tag == TagDate) return toLocalDate(DefaultUnserializer.readDateTime(reader, stream));
-        if (tag == TagTime) return toLocalDate(DefaultUnserializer.readTime(reader, stream));
-        if (tag == TagNull || tag == TagEmpty) return null;
-        if (tag == TagRef) return toLocalDate(reader.readRef(stream));
         switch (tag) {
-            case '0': return LocalDate.ofEpochDay(0l);
-            case '1': return LocalDate.ofEpochDay(1l);
-            case '2': return LocalDate.ofEpochDay(2l);
-            case '3': return LocalDate.ofEpochDay(3l);
-            case '4': return LocalDate.ofEpochDay(4l);
-            case '5': return LocalDate.ofEpochDay(5l);
-            case '6': return LocalDate.ofEpochDay(6l);
-            case '7': return LocalDate.ofEpochDay(7l);
-            case '8': return LocalDate.ofEpochDay(8l);
-            case '9': return LocalDate.ofEpochDay(9l);
+            case TagDate: return toLocalDate(DefaultUnserializer.readDateTime(reader, stream));
+            case TagTime: return toLocalDate(DefaultUnserializer.readTime(reader, stream));
+            case TagNull:
+            case TagEmpty: return null;
+            case TagString: return LocalDate.parse(StringUnserializer.readString(reader, stream));
+            case TagRef: return toLocalDate(reader.readRef(stream));
+        }
+        if (tag >= '0' && tag <= '9') return LocalDate.ofEpochDay(tag - '0');
+        switch (tag) {
             case TagInteger:
             case TagLong: return LocalDate.ofEpochDay(ValueReader.readLong(stream));
             case TagDouble: return LocalDate.ofEpochDay(Double.valueOf(ValueReader.readDouble(stream)).longValue());
-            case TagString: return LocalDate.parse(StringUnserializer.readString(reader, stream));
             default: throw ValueReader.castError(reader.tagToString(tag), LocalDate.class);
         }
     }

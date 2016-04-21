abf8d76a76e28eff608f69cec649f7a6e759bd64
Ma Bingyao <mabingyao@gmail.com>
2016-04-21 23:18:47 +0800

    Added Idempotent

---
+++ src/main/java/hprose/common/Idempotent.java
@@ -0,0 +1,30 @@
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Idempotent.java                                        *
+ *                                                        *
+ * Idempotent Annotation for Java.                        *
+ *                                                        *
+ * LastModified: Apr 21, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+package hprose.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Idempotent {
+    boolean value() default true;
+}

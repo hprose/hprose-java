0a684a41a90b7892a0fdb08e56e1cc1ac57dc82e
Ma Bingyao <mabingyao@gmail.com>
2016-04-21 23:18:22 +0800

    Added Failswitch

---
+++ src/main/java/hprose/common/Failswitch.java
@@ -0,0 +1,30 @@
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+/**********************************************************\
+ *                                                        *
+ * Failswitch.java                                        *
+ *                                                        *
+ * Failswitch Annotation for Java.                        *
+ *                                                        *
+ * LastModified: Apr 21, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+package hprose.common;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Failswitch {
+    boolean value() default true;
+}

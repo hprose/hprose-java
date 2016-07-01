582bc3bb996567efd31b0b67395e9b23073901f7
Ma Bingyao <mabingyao@gmail.com>
2016-07-01 16:16:14 +0800

    Update TimePushServer.java

--- example/Example/src/main/java/hprose/example/push/TimePushServer.java
+++ example/Example/src/main/java/hprose/example/push/TimePushServer.java
@@ -4,15 +4,14 @@ import hprose.server.HproseTcpServer;
 import hprose.util.concurrent.Timer;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Calendar;
 
 public class TimePushServer {
     public static void main(String[] args) throws URISyntaxException, IOException {
         HproseTcpServer server = new HproseTcpServer("tcp://0.0.0.0:8080");
         server.publish("time");
         server.start();
-        Timer timer = new Timer(
-                () -> server.push("time", java.util.Calendar.getInstance())
-        );
+        Timer timer = new Timer(() -> server.push("time", Calendar.getInstance()));
         timer.setInterval(1000);
         System.out.println("START");
         System.in.read();

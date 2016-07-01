ca741f1a3c16211155c7ea3c21da345af7a266f8
Ma Bingyao <mabingyao@gmail.com>
2016-07-01 10:25:39 +0800

    Added Broadcast example.

---
+++ example/Example/src/main/java/hprose/example/push/BroadcastClient.java
@@ -0,0 +1,22 @@
+package hprose.example.push;
+
+import hprose.client.HproseClient;
+import hprose.util.concurrent.Action;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+interface IBroadcast {
+    public void news(Action<String> callback);
+    public String hello(String name);
+}
+
+public class BroadcastClient {
+    public static void main(String[] args) throws IOException, URISyntaxException, Throwable {
+        final HproseClient client = HproseClient.create("tcp://127.0.0.1:8081");
+        IBroadcast bc = client.useService(IBroadcast.class);
+        bc.news((String news) -> System.out.println(news));
+        Thread.sleep(100);
+        System.out.println(bc.hello("Hprose"));
+        Thread.sleep(10000);
+    }
+}
---
+++ example/Example/src/main/java/hprose/example/push/BroadcastServer.java
@@ -0,0 +1,27 @@
+package hprose.example.push;
+
+import hprose.common.HproseException;
+import hprose.server.HproseTcpServer;
+import hprose.server.TcpContext;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+public class BroadcastServer {
+    public static String hello(String name) throws HproseException {
+        TcpContext context = HproseTcpServer.getCurrentContext();
+        System.out.println(Arrays.toString(context.clients.idlist("news")));
+        context.clients.broadcast("news", "this is a pushed message:" + name);
+        return "Hello " + name + "! -- " + context.getSocket().getRemoteSocketAddress().toString();
+    }
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        HproseTcpServer server = new HproseTcpServer("tcp://0.0.0.0:8081");
+        server.add("hello", BroadcastServer.class);
+        server.publish("news");
+        server.start();
+        System.out.println("START");
+        System.in.read();
+        server.stop();
+        System.out.println("STOP");
+    }
+}
